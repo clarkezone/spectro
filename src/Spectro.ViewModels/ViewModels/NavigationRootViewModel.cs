0a45cdc394ce33f44f7bfd976a016fee24622d9a
James Clarke (OS GROUP) <jeclarke@ntdev.microsoft.com>
2017-07-27 09:29:29 -0700

    wip

--- src/Spectro.ViewModels/ViewModels/NavigationRootViewModel.cs
+++ src/Spectro.ViewModels/ViewModels/NavigationRootViewModel.cs
@@ -1,9 +1,27 @@
 ﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using System;
 
 namespace Spectro.ViewModels
 {
     public class NavigationRootViewModel : ViewModelBase
     {
+        private ObservableCollection<object> _menuItems = new ObservableCollection<object>();
 
+        public NavigationRootViewModel()
+        {
+            BuildMenuItems();
+        }
+
+        public ObservableCollection<object> NavigationMenuItems
+        {
+            get => _menuItems;
+        }
+
+        private void BuildMenuItems()
+        {
+            _menuItems.Add("One");
+            _menuItems.Add("Two");
+        }
     }
 }
