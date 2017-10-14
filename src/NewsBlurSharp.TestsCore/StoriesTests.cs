218eeb9d50bb3634bf8a56c9320ec48d04fef95a
Xivolkar <ajaykelkar@outlook.com>
2017-10-15 00:56:49 +0530

    Adding wrapper for marking stories as Read/Unread endpoint

--- src/NewsBlurSharp.TestsCore/StoriesTests.cs
+++ src/NewsBlurSharp.TestsCore/StoriesTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace NewsBlurSharp.Tests
@@ -19,6 +21,27 @@ namespace NewsBlurSharp.Tests
             var stories = await _subject.GetStoriesAsync(19354);
 
             Assert.NotNull(stories);
+        }
+
+        [Fact]
+        public async Task MarkStoryAsUnread()
+        {
+            String storyHash = "19354:57abe7";
+
+            var response = await _subject.MarkStoryUnreadAsync(storyHash);
+            Assert.NotNull(response);
+            Console.WriteLine(response.ToString());
+        }
+
+        [Fact]
+        public async Task MarkStoryAsRead()
+        {
+            List<String> hashes = new List<string>();
+            hashes.Add("19354:57abe7");
+
+            var response = await _subject.MarkStoriesReadAsync(hashes);
+            Assert.NotNull(response);
+            Console.WriteLine(response.ToString());
         }
     }
 }
