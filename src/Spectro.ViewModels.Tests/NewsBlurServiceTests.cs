e119b700f029456de59f3165bd7f20b6bb749cd5
James Clarke <james@clarkezone.net>
2017-10-29 11:03:04 +0100

    Add test project for viewmodels

---
+++ src/Spectro.ViewModels.Tests/NewsBlurServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spectro.Core.Interfaces;
+using Moq;
+using System.Threading.Tasks;
+
+namespace Spectro.ViewModels.Tests
+{
+    [TestClass]
+    public class NavigationRootViewmodelTests
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            //TODO this doesn't pass 
+            var mockCreds = new Mock<ICredentialsPrompt>();
+            mockCreds.Setup(mc => mc.GetUsernamePassword()).Returns(("un","pw"));
+
+            var mock = new Mock<INewsBlurService>();
+            
+            mock.Setup(fw => fw.Login(mockCreds.Object)).Returns(Task.CompletedTask);
+            
+            NavigationRootViewModel model = new NavigationRootViewModel(mock.Object, null);
+            model.LoginLogoutCommand.Execute(null);
+        }
+    }
+}
